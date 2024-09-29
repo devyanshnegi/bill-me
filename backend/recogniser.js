38480a3f2df98ad00e0d365b42fbaa682222856e
Soham-1827 <sohamchoulwar.27@gmail.com>
2024-09-28 23:36:26 -0700

    Made changes to recogniser and deleted parser.js

--- backend/recogniser.js
+++ backend/recogniser.js
@@ -1,19 +1,51 @@
-import { ImageAnnotatorClient } from '@google-cloud/vision';
-import fs from 'fs/promises'
-// Creates a client
-const client = new ImageAnnotatorClient();
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
-/**
- * TODO(developer): Uncomment the following line before running the sample.
- */
- const fileName = './bill.jpg';
-try{
-    const [result] = await client.textDetection(fileName);
-    const detections = result.textAnnotations;
-    var dictstring = JSON.stringify(detections);
-    await fs.writeFile("output.json", dictstring);
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
+
+const generationConfig = {
+  temperature: 0.8,
+  topP: 1,
+  topK: 1,
+  maxOutputTokens: 2048,
+};
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+  generationConfig,
+});
+
+async function generateContent(imageData) {
+  const imageFormat = imageData.toString("base64");
+  const parts = [
+    {
+      text: "What is the text in the receipt/bill and output it as a json with parameters product name, product price and in the end the total price of the bill?",
+    },
+    {
+      inlineData: {
+        mimeType: "image/jpeg",
+        data: imageFormat,
+      },
+    },
+  ];
+  const data = await model.generateContent({
+    contents: [{ role: "user", parts }],
+  });
+
+  console.log("Generated Data:", data);
+
+  const result = await data.response;
+  const text = await result.text();
+  return text;
 }
-// Performs text detection on the local file
-catch(error){
-    alert('Error: ' + error.message)
+
+export async function POST(req) {
+  try {
+    const { image } = await req.json();
+    const buffer = Buffer.from(image.data, "base64");
+    const recognizedText = await generateContent(buffer);
+    return NextResponse.json({ response: { text: recognizedText } });
+  } catch (error) {
+    console.error("Error in API:", error.message);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
 }
